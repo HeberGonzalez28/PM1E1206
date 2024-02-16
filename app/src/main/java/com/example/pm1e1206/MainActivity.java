b00c8b3a603f800f69dc237c06ed7740a195d57a
denni <dennislandero40@gmail.com>
2024-02-15 22:19:09 -0600

    programar botones para ir de un activity a otro

--- app/src/main/java/com/example/pm1e1206/MainActivity.java
+++ app/src/main/java/com/example/pm1e1206/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.pm1e1206;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnContactos,btnAgregar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAgregar = (Button) findViewById(R.id.btnagregarcontacto);
+        btnContactos = (Button) findViewById(R.id.btnvercontactos);
+
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ActivityAgregar.class);
+                startActivity(intent);
+            }
+        });
+
+        btnContactos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ActivityVerLista.class);
+                startActivity(intent);
+            }
+        });
     }
 }

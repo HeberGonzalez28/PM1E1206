b441b8d95e4b0e5e2bc685ec4b8eb83a0c8436de
HeberGonzalez28 <ever.gonzalez83@gmail.com>
2024-02-16 19:18:01 -0600

    Avance 16-2-24/2.5

---
+++ app/src/main/java/Models/Pais.java
@@ -0,0 +1,27 @@
+package Models;
+
+public class Pais {
+    private int id;
+    private String pais;
+
+    public Pais(int id, String pais) {
+        this.id = id;
+        this.pais = pais;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
--- app/src/main/java/com/example/pm1e1206/ActivityContacto.java
+++ app/src/main/java/com/example/pm1e1206/ActivityContacto.java
@@ -1,14 +1,37 @@
 package com.example.pm1e1206;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import Configuracion.SQLiteConexion;
+import Configuracion.Transacciones;
 
 public class ActivityContacto extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+
+
+
+
+
+
+
     }
+
+
 }
